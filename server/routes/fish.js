fcedbf9c854dacad04dc69a4f70c49e43a39dc19
richard <richard@nobrolla.com>
2020-07-05 18:41:55 -0600

    Add fish route

---
+++ server/routes/fish.js
@@ -0,0 +1,35 @@
+var db = require("../db/db");
+
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const sql = "select * from fish";
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+router.put("/:id", function (req, res, next) {
+  console.log(`update ${req.params.id} ${req.body.status}`);
+  let sql = `UPDATE fish
+            SET status = ?
+            WHERE id = ?`;
+
+  db.all(sql, [req.body.status, req.params.id], (err) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.status(200).send();
+  });
+
+});
+
+module.exports = router;
